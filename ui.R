26b72db9baf2c0335490b4bd0b2e42f0660eb472
Gonçalo Matos <gon1995matos@gmail.com>
2017-04-29 15:01:46 +0100

    main files created

---
+++ ui.R
@@ -0,0 +1,24 @@
+getSurfaceWater <- function(xParam, yParam, myList) { 
+  #serverRequest
+  #generate list
+  addValues(myList, myValue)
+}
+
+getGroundWater <- function(xParam, yParam, myList) { 
+  #serverRequest
+  #generate list
+  addValues(myList, myValue) 
+}
+
+getWaterQuality <- function(xParam, yParam, myList) { 
+  #serverRequest
+  #generate list
+  addValues(myList, myValue)
+}
+
+#AuxFuncs
+
+addValues <- function(myList, myValue) { 
+  #generate result 
+}
+
